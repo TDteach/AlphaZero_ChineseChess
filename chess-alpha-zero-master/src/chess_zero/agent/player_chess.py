86e089c7ea6308ad2d6db5f5307d59e171750c18
tdteach <ditang@ie.cuhk.edu.hk>
2018-01-16 13:39:31 +0800

    policy

--- chess-alpha-zero-master/src/chess_zero/agent/player_chess.py
+++ chess-alpha-zero-master/src/chess_zero/agent/player_chess.py
@@ -221,7 +221,9 @@ class ChessPlayer:
 
     def apply_temperature(self, policy, turn):
         tau = np.power(self.play_config.tau_decay_rate, turn + 1)
-        if tau < 0.1 or turn > 30:
+        if turn > 100:
+            tau -=0.3
+        if tau < 0.1:
             tau = 0
         if tau == 0:
             action = np.argmax(policy)
